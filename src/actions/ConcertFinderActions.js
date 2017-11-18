40aae8de08b1db764cdbef4300d4175116bd5483
Naoki Yamaguchi <naoki@Naokis-MacBook-Pro.local>
2017-11-18 15:07:51 -0500

    firebase testing

--- src/actions/ConcertFinderActions.js
+++ src/actions/ConcertFinderActions.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { firebaseConfig } from '../../config';
+// import { firebaseConfig } from '../../config';
 import {
 	CONCERT_ARTIST_CHANGED
 	} from '../actions/types';
@@ -16,7 +16,9 @@ export const firebaseArtistSearch = () => {
 	// firebase.initializeApp(firebaseConfig); this doesn't work
 	// produces error "Firebase App already exists"
 
-	// the code below produces the error 
+	// the code below produces the error
+	// undefined is not an object (evaluating '_firebase.firebase.database')
+	// maybe passing firebase as a prop with help
 	firebase.database().ref('artistSearch/').once('value', (snapshot) => {
 		console.log('testing snapshot: ', snapshot);
 	});
