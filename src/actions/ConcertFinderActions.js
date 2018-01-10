f85134bb6df7a6f0f19d3ed42c36236d51400270
Naoki Yamaguchi <naoki@Naokis-MacBook-Pro.local>
2018-01-09 21:12:25 -0500

    got asynch redux thunk stuff working with firebase

--- src/actions/ConcertFinderActions.js
+++ src/actions/ConcertFinderActions.js
@@ -13,33 +13,24 @@ export const artistInputChanged = (text) => {
 };
 
 export const artistSearch = (text) => {
-		const searchStrings = buildSearchString(text);
-		console.log(searchStrings.startString);
-		console.log(searchStrings.endString);
 		return dispatch => {
-			firebaseArtistSearch(searchStrings.startString, searchStrings.endString)
+			// search using the inputted text as the startString
+			// and text + zzzz as the end string to limit firebase search
+			firebaseArtistSearch(text, `${text}zzzzz`)
 			.then(snapshot => {
-				console.log(snapshot.val());
 				dispatch({
 					type: CONCERT_ARTIST_SEARCH_SUCCESS,
 					payload: snapshot.val()
 				});
 			});
 		};
-		// getData(searchStrings.startString, searchStrings.endString);
-		// return {
-		// 	type: CONCERT_ARTIST_SEARCH_SUCCESS,
-		// 	payload: searchStrings.startString
-		// };
 };
-//
-const firebaseArtistSearch = ({ startString, endString }) => {
+
+const firebaseArtistSearch = (startString, endString) => {
 		const ref = firebase.database().ref('artistSearch');
-		console.log(startString);
-		console.log(endString);
 		return ref.orderByKey()
-			.startAt('ph')//(startString)
-			.endAt('phzzz')//(endString)
+			.startAt(startString)
+			.endAt(endString)
 			.once('value');
 	// try {
 	// } catch (error) {
@@ -47,52 +38,13 @@ const firebaseArtistSearch = ({ startString, endString }) => {
 	// }
 };
 //
-export const getData = ({ startString, endString }) => {
-	return dispatch => {
-		firebaseArtistSearch(startString, endString)
-		.then(snapshot => {
-			console.log(snapshot.val());
-			dispatch({
-				type: CONCERT_ARTIST_SEARCH_SUCCESS,
-				payload: snapshot.val()
-			});
-		});
-	};
-};
-// export const artistChanged = (text) => {
-// 	const searchStrings = buildSearchString(text);
-// 	const artistSearchData = firebaseArtistSearch(
-// 		searchStrings.startString,
-// 		searchStrings.endString,
-// 		(err, result) => { console.log(result); }
-// 	);
-// 	return {
-// 		type: CONCERT_ARTIST_CHANGED,
-// 		payload: artistSearchData
-// 	};
+// const buildSearchString = (text) => {
+// 		// Firebase uses a startAt and endAt function to
+// 		// search keys so take user input and create endString
+// 		const startString = text;
+// 		const endString = `${text}zzzzz`;
+// 		return {
+// 			startString,
+// 			endString
+// 		};
 // };
-
-// const firebaseArtistSearch = (startString, endString, callback) => {
-// 	const ref = firebase.database().ref('artistSearch');
-//
-// 	ref.orderByKey()
-// 	.startAt(startString)
-// 	.endAt(endString)
-// 	.once('value', snapshot => {
-// 		const data = snapshot.val();
-// 			callback(null, data);
-// 	}, error => {
-// 		console.log(error);
-// 	});
-// };
-//
-const buildSearchString = (text) => {
-		// Firebase uses a startAt and endAt function to
-		// search keys so take user input and create endString
-		const startString = text;
-		const endString = `${text}zzzzz`;
-		return {
-			startString,
-			endString
-		};
-};
